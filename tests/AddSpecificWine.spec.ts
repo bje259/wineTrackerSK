2b99c2f735c59424f92f59d71ece773dd36626d1
Vdain <bradley.euell@gmail.com>
2023-11-30 05:00:53 -0600

    Testing setup

---
+++ tests/AddSpecificWine.spec.ts
@@ -0,0 +1,317 @@
+// @ts-ignore
+import { WineCellar } from '$lib/WineCellar';
+import { myWineCellar } from '$lib/store';
+import type { Cellar, Wine } from '$lib/types';
+import { beforeEach, describe, expect, test } from 'vitest';
+
+/**
+ * @description boolean array for controlling the state of various elements in the UI
+ * @param fOn[0] - openNewWine
+ * @param fOn[1] - newProducerMode
+ * @param fOn[2] - newWineName > 0
+ * @param fOn[3] - newWineVintage > 1980 && newWineVintage < 2024
+ * @param fOn[4] - newWineQty > 0
+ * @param fOn[5] -
+ * @param fOn[6] -
+ * @param fOn[7] -
+ */
+let fOn: boolean[] = [
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false
+];
+
+let openNewWine = false;
+let newProducerMode = false;
+let newProducerName = '';
+let newWineName = '';
+let newWineProducer = '';
+let newWineVariety = '';
+let newVineyardLocation = '';
+let newWineVintage = 0;
+let newWineBin = '';
+let newWineQty = 0;
+let newWinePurchased = '';
+let newWineNotes = '';
+let producer = '';
+let tempWines: Wine[] = [];
+let newWine: Wine;
+let wineName = '';
+let variety = '';
+let vintage = 0;
+let bin = '';
+let qty = 0;
+let purchased = '';
+let notes = '';
+let wine: Wine;
+let wineCellar: Cellar;
+let producers: string[];
+let wines: Wine[];
+let wineNames: string[];
+let wineVarieties: string[];
+let wineVintages: number[];
+let wineBins: string[];
+let wineQtys: number[];
+let winePurchaseds: string[];
+let wineNotes: string[];
+let ownedWineCellar = new WineCellar();
+const unsubscribe = myWineCellar.subscribe((value) => {
+	ownedWineCellar = value;
+});
+
+let dispatch = (type: string, detail?: any) => {
+	document.dispatchEvent(
+		new CustomEvent(type, {
+			detail
+		})
+	);
+};
+
+/**
+ * @description function to check the state of various elements in the UI using OR, AND and NOT logic on the fOn array.  Negative indices have the not (!) operator applied to the value mapped to the ABS value of the index.
+ * @param indices - array of indices to check
+ * @param useOr - boolean to indicate whether to use OR logic (true) or AND logic (false)
+ * @returns boolean
+ * @see fOn[] index assignements
+ * @param fOn[0] - openNewWine
+ * @param fOn[1] - newProducerMode
+ * @param fOn[2] - newWineName > 0
+ * @param fOn[3] - newWineVintage > 1980 && newWineVintage < 2024
+ * @param fOn[4] - newWineQty > 0
+ * @example
+ * // returns true if openNewWine is true OR newProducerMode is true
+ * chk([0,1], true)
+ *  // returns true if openNewWine is true AND newProducerMode is true
+ * chk([0,1], false)
+ * // returns true if openNewWine is true AND newProducerMode is false
+ * chk([0,-1], false)
+ * // returns true if openNewWine is false AND newProducerMode is true
+ * chk([-0,1], false)
+ * // returns true if openNewWine is false AND newProducerMode is false
+ * chk([-0,-1], false)
+ *
+ */
+function chk(indices: number[], useOr = false) {
+	if (useOr) {
+		// OR logic - returns true if any condition is true
+		return indices.some((index) => {
+			return Object.is(Math.abs(index), index) ? fOn[index] : !fOn[Math.abs(index)];
+		});
+	} else {
+		// AND logic - returns true if all conditions are true
+		return indices.every((index) => {
+			return Object.is(Math.abs(index), index) ? fOn[index] : !fOn[Math.abs(index)];
+		});
+	}
+}
+
+describe('Tests function controlling logic checks for UI form', () => {
+	test('chk - OR logic, openNewWine is true', () => {
+		fOn[0] = true;
+		fOn[1] = false;
+		expect(chk([0, 1], true)).toBe(true);
+	});
+
+	test('chk - OR logic, newProducerMode is true', () => {
+		fOn[0] = false;
+		fOn[1] = true;
+		expect(chk([0, 1], true)).toBe(true);
+	});
+
+	test('chk - OR logic, openNewWine and newProducerMode are false', () => {
+		fOn[0] = false;
+		fOn[1] = false;
+		expect(chk([0, 1], true)).toBe(false);
+	});
+
+	test('chk - AND logic, openNewWine and newProducerMode are true', () => {
+		fOn[0] = true;
+		fOn[1] = true;
+		expect(chk([0, 1], false)).toBe(true);
+	});
+
+	test('chk - AND logic, openNewWine is true and newProducerMode is false', () => {
+		fOn[0] = true;
+		fOn[1] = false;
+		expect(chk([0, -1], false)).toBe(true);
+	});
+
+	test('chk - AND logic, openNewWine is false and newProducerMode is true', () => {
+		fOn[0] = false;
+		fOn[1] = true;
+		expect(chk([-0, 1], false)).toBe(true);
+	});
+
+	test('chk - AND logic, openNewWine and newProducerMode are false; Note with negative indices, and both flags flase, I expect the expression to check - is fOn[0] false and fOn[1] false?', () => {
+		fOn[0] = false;
+		fOn[1] = false;
+		expect(chk([-0, -1], false)).toBe(true);
+	});
+
+	test('chk - AND logic, openNewWine is true and newProducerMode is false', () => {
+		fOn[0] = true;
+		fOn[1] = false;
+		expect(chk([-0, -1], false)).toBe(false);
+	});
+});
+
+/*let newWine: Wine = {
+		"Wine Name": "Margaux 2015",
+		"Vineyard Location": "Bordeaux, France",
+		Variety: "Cabernet Sauvignon",
+		Vintage: 2015,
+		Bin: "A1",
+		Qty: 10,
+		Purchased: "2020-01-01",
+		Notes: "Excellent vintage"
+	};*/
+
+function newProducerSubmit() {
+	console.log(`ZZ Wine newProducerSubmit - newProducer: ${newProducerName}`);
+	let storedValue = localStorage.getItem(newProducerName) ?? '[]';
+	if (storedValue === '[]') {
+		console.log(
+			`ZZ Wine newProducerNameSubmit - newProducerName: ${newProducerName} - no wines found in local storage - creating new entry`
+		);
+		localStorage.setItem(newProducerName, JSON.stringify([]));
+		ownedWineCellar.addProducer(newProducerName);
+	}
+	console.log(`Updated myWineCellar with new producer ${newProducerName}`);
+	dispatch('wineUpdated');
+	console.log('dispatched wineUpdated');
+	producer = newProducerName;
+}
+openNewWine = true;
+newProducerName = '';
+
+describe('AddSpecificWine intends to allow user interaction to add wins to the cellar.  Tests here will be focused on utility functions within the svelte component', () => {
+	const producer = 'Chateau Margaux';
+	const wine: Wine = {
+		'Wine Name': 'Margaux 2015',
+		'Vineyard Location': 'Bordeaux, France',
+		Variety: 'Cabernet Sauvignon',
+		Vintage: 2015,
+		Bin: 'A1',
+		Qty: 10,
+		Purchased: '2020-01-01',
+		Notes: 'Excellent vintage'
+	};
+	beforeEach(() => {});
+
+	test('WineCellar should be defined', () => {
+		expect(ownedWineCellar).toBeDefined();
+	});
+
+	test('WineCellar should have a producers array', () => {
+		expect(ownedWineCellar.getProducerNames()).toBeDefined();
+	});
+
+	test('myWineCellar should have a wines array', () => {
+		expect(ownedWineCellar.getAllWinesFlat()).toBeDefined();
+	});
+
+	test(' I should be able to submit a new producer', () => {
+		newProducerName = producer;
+		newProducerSubmit();
+		expect(ownedWineCellar.getProducerNames().some( (producer) => producer.name === newProducerName)).toBe(true);
+	});
+});
+
+function clearText(f: (v: string) => void) {
+    f('');
+}
+
+function newWineSubmit() {
+	console.log(`ZZ Wine newWineSubmit - producer: ${producer} - newWine:`);
+	console.log(newWine);
+	console.log(`ZZ Wine newWineSubmit - newWine.Qty: ${newWine.Qty}`);
+
+	let storedValue = localStorage.getItem(producer) ?? '[]';
+	try {
+		tempWines = JSON.parse(storedValue) as Wine[];
+		console.log(`ZZ Wine newWineSubmit - pulled producer's wines from local storage:`);
+		console.log(tempWines);
+		myWineCellar.update((current) => {
+			current.updateCellarByProducer(producer, tempWines);
+			return current;
+		});
+		console.log('Updated myWineCellar with tempWines');
+		console.log(ownedWineCellar.getWinesByProducer(producer));
+		myWineCellar.update((current) => {
+			current.addWine(producer, newWine);
+			return current;
+		});
+		console.log(`Updated myWineCellar with newWine
+				${newWine['Wine Name']}
+				${newWine.Qty}`);
+		console.log('New myWineCellar wine entry:');
+		console.log(ownedWineCellar.getWinesByProducerWineName(producer, newWine['Wine Name']));
+		console.log('updating local storage');
+		localStorage.setItem(producer, JSON.stringify(ownedWineCellar.getWinesByProducer(producer)));
+		console.log('dispatching wineUpdated');
+		dispatch('wineUpdated');
+		console.log('dispatched wineUpdated');
+	} catch (error) {
+		console.error('Error parsing JSON from localStorage', error);
+		tempWines = [];
+	}
+	clearText((v) => (newWineName = v));
+	clearText((v) => (newVineyardLocation = v));
+	clearText((v) => (newWineVariety = v));
+	newWineVintage = 2020;
+	newWineQty = 1;
+	clearText((v) => (newWineBin = v));
+	clearText((v) => (newWinePurchased = v));
+	clearText((v) => (newWineNotes = v));
+	openNewWine = false;
+}
+
+describe('Testing the newWineSubmit form functions', () => {
+    newWine = {
+        'Wine Name': 'Margaux 2015',
+        'Vineyard Location': 'Bordeaux, France',
+        Variety: 'Cabernet Sauvignon',
+        Vintage: 2015,
+        Bin: 'A1',
+        Qty: 10,
+        Purchased: '2020-01-01',
+        Notes: 'Excellent vintage'
+    };
+    producer = 'Chateau Margaux';
+    beforeEach(() => {
+        newWineName = newWine['Wine Name'];
+        newVineyardLocation = newWine['Vineyard Location'];
+        newWineVariety = newWine.Variety;
+        newWineVintage = newWine.Vintage;
+        newWineBin = newWine.Bin;
+        newWineQty = newWine.Qty;
+        newWinePurchased = newWine.Purchased;
+        newWineNotes = newWine.Notes;
+    });
+
+test('I should be able to submit a new wine', () => {
+    newWineSubmit();
+    expect(ownedWineCellar.getWinesByProducerWineName(producer, newWineName)).toBeDefined();
+}
+);
+
+test('I should be able to submit a new wine', () => {
+    newWineSubmit();
+    expect(ownedWineCellar.getWinesByProducerWineName(producer, newWineName)).toBeDefined();
+}
+)
+
+
+});
---
+++ tests/WineCellar.spec.ts
@@ -0,0 +1,388 @@
+import { beforeEach, describe, expect, it, test } from 'vitest';
+import { WineCellar } from '$lib/WineCellar';
+import type { Cellar, Wine } from '$lib/types';
+
+describe('WineCellar', () => {
+	let wineCellar: WineCellar;
+	const producer = 'Chateau Margaux';
+	const wine: Wine = {
+		'Wine Name': 'Margaux 2015',
+		'Vineyard Location': 'Bordeaux, France',
+		Variety: 'Cabernet Sauvignon',
+		Vintage: 2015,
+		Bin: 'A1',
+		Qty: 10,
+		Purchased: '2020-01-01',
+		Notes: 'Excellent vintage'
+	};
+	beforeEach(() => {
+		wineCellar = new WineCellar({});
+	});
+
+	test('addWine should add wine to the cellar', () => {
+		wineCellar.addWine(producer, wine);
+		expect(wineCellar.cellar[producer]).toHaveLength(1);
+		expect(wineCellar.cellar[producer][0]).toEqual(wine);
+	});
+
+	test('addWine should add wine to the existing producer in the cellar', () => {
+		const producerA = 'Producer A';
+		const wine1 = { 'Wine Name': 'Wine A', Vintage: 2020, Qty: 1 };
+		const wine2 = { 'Wine Name': 'Wine B', Vintage: 2021, Qty: 2 };
+
+		wineCellar.addWine(producerA, wine1);
+		wineCellar.addWine(producerA, wine2);
+
+		expect(wineCellar.cellar[producerA]).toHaveLength(2);
+		expect(wineCellar.cellar[producerA][0]).toEqual(wine1);
+		expect(wineCellar.cellar[producerA][1]).toEqual(wine2);
+	});
+
+	test('removeWine should remove wine from the cellar', () => {
+		wineCellar.addWine(producer, wine);
+		const result = wineCellar.removeWine(producer, wine['Wine Name'],wine);
+		expect(result).toBe(true);
+		expect(wineCellar.cellar[producer]).toHaveLength(0);
+	});
+
+	test('removeWine should return false if wine is not found in the cellar', () => {
+		const result = wineCellar.removeWine(producer, wine['Wine Name']);
+		expect(result).toBe(false);
+	});
+
+	test('getWinesByProducer should return wines produced by the specified producer', () => {
+		wineCellar.addWine(producer, wine);
+		const wines = wineCellar.getWinesByProducer(producer);
+		expect(wines).toHaveLength(1);
+		expect(wines[0]).toEqual(wine);
+	});
+
+	test('getWinesByProducer should return undefined if no wines are found', () => {
+		const wines = wineCellar.getWinesByProducer(producer);
+		expect(wines).toBeUndefined();
+	});
+
+	test('getCellar should return the cellar object', () => {
+		wineCellar.addWine(producer, wine);
+		const cellar = wineCellar.getCellar();
+		expect(cellar).toEqual({ [producer]: [wine] });
+	});
+
+	test('checkWineByNameVintage should return the wine object if found', () => {
+		wineCellar.addWine(producer, wine);
+		const foundWine = wineCellar.checkWineByNameVintage(wine['Wine Name'], wine.Vintage);
+		expect(foundWine).toEqual(wine);
+	});
+
+	test('checkWineByNameVintage should return undefined if wine is not found', () => {
+		const foundWine = wineCellar.checkWineByNameVintage(wine['Wine Name'], wine.Vintage);
+		expect(foundWine).toBeUndefined();
+	});
+});
+
+describe('WineCellar Filter tests', () => {
+	let loadWinesString = `{
+	"B.R. Cohn": [
+		{
+			"Wine Name": "Cabernet Sauvignon",
+			"Vineyard Location": "North Coast",
+			"Variety": "Cabernet Sauvignon",
+			"Vintage": 2013,
+			"Bin": "8-3",
+			"Qty": 1,
+			"Purchased": "",
+			"Notes": ""
+		}
+	],
+	"Barons De Rothschild (Lafite)": [
+		{
+			"Wine Name": "Saint Emilion Reserve Speciale",
+			"Vineyard Location": "Saint Emilion",
+			"Variety": "Red Blend",
+			"Vintage": 2012,
+			"Bin": "5-1",
+			"Qty": 1,
+			"Purchased": "",
+			"Notes": ""
+		}
+	],
+	"Becker Vineyards": [
+		{
+			"Wine Name": "Barbera ",
+			"Vineyard Location": "Texas Hill Country",
+			"Variety": "Barbera",
+			"Vintage": 2020,
+			"Bin": "12-2",
+			"Qty": 1,
+			"Purchased": "10/24/2023",
+			"Notes": ""
+		},
+		{
+			"Wine Name": "Barbera - Merlot",
+			"Vineyard Location": "Texas  ",
+			"Variety": "Red Blend",
+			"Vintage": 2012,
+			"Bin": "15-2",
+			"Qty": 1,
+			"Purchased": "",
+			"Notes": ""
+		}
+	]
+}`;
+	let loadedOwnedWines: Cellar = {};
+
+	try {
+		loadedOwnedWines = JSON.parse(loadWinesString) as Cellar;
+	} catch (error) {
+		console.error(`Error parsing data from demo string:`, error);
+	}
+
+	const myCellar = new WineCellar(loadedOwnedWines);
+
+	it('should filter the cellar by producer', () => {
+		const filteredCellar = myCellar.getFilteredCellar({ producer: 'Becker Vineyards' });
+
+		expect(filteredCellar).toEqual({
+			'Becker Vineyards': [
+				{
+					'Wine Name': 'Barbera ',
+					'Vineyard Location': 'Texas Hill Country',
+					Variety: 'Barbera',
+					Vintage: 2020,
+					Bin: '12-2',
+					Qty: 1,
+					Purchased: '10/24/2023',
+					Notes: ''
+				},
+				{
+					'Wine Name': 'Barbera - Merlot',
+					'Vineyard Location': 'Texas  ',
+					Variety: 'Red Blend',
+					Vintage: 2012,
+					Bin: '15-2',
+					Qty: 1,
+					Purchased: '',
+					Notes: ''
+				}
+			]
+		});
+	});
+
+	it('should filter the cellar by variety', () => {
+		const filteredCellar = myCellar.getFilteredCellar({ variety: 'Cabernet Sauvignon' });
+
+		expect(filteredCellar).toEqual({
+			'B.R. Cohn': [
+				{
+					'Wine Name': 'Cabernet Sauvignon',
+					'Vineyard Location': 'North Coast',
+					Variety: 'Cabernet Sauvignon',
+					Vintage: 2013,
+					Bin: '8-3',
+					Qty: 1,
+					Purchased: '',
+					Notes: ''
+				}
+			]
+		});
+	});
+
+	it('should filter the cellar by vineyard location', () => {
+		const filteredCellar = myCellar.getFilteredCellar({ vineyard: 'Texas Hill Country' });
+
+		expect(filteredCellar).toEqual({
+			'Becker Vineyards': [
+				{
+					'Wine Name': 'Barbera ',
+					'Vineyard Location': 'Texas Hill Country',
+					Variety: 'Barbera',
+					Vintage: 2020,
+					Bin: '12-2',
+					Qty: 1,
+					Purchased: '10/24/2023',
+					Notes: ''
+				}
+			]
+		});
+	});
+
+	it('should filter the cellar by search term ', () => {
+		const filteredCellar = myCellar.getFilteredCellar({ searchterm: 'Barbera' });
+
+		expect(filteredCellar).toEqual({
+			'Becker Vineyards': [
+				{
+					'Wine Name': 'Barbera ',
+					'Vineyard Location': 'Texas Hill Country',
+					Variety: 'Barbera',
+					Vintage: 2020,
+					Bin: '12-2',
+					Qty: 1,
+					Purchased: '10/24/2023',
+					Notes: ''
+				},
+				{
+					'Wine Name': 'Barbera - Merlot',
+					'Vineyard Location': 'Texas  ',
+					Variety: 'Red Blend',
+					Vintage: 2012,
+					Bin: '15-2',
+					Qty: 1,
+					Purchased: '',
+					Notes: ''
+				}
+			]
+		});
+	});
+	it('should filter the cellar by vineyard searchterm ', () => {
+		const filteredCellar = myCellar.getFilteredCellar({ searchterm: 'texas' });
+
+		expect(filteredCellar).toEqual({
+			'Becker Vineyards': [
+				{
+					'Wine Name': 'Barbera ',
+					'Vineyard Location': 'Texas Hill Country',
+					Variety: 'Barbera',
+					Vintage: 2020,
+					Bin: '12-2',
+					Qty: 1,
+					Purchased: '10/24/2023',
+					Notes: ''
+				},
+				{
+					'Wine Name': 'Barbera - Merlot',
+					'Vineyard Location': 'Texas  ',
+					Variety: 'Red Blend',
+					Vintage: 2012,
+					Bin: '15-2',
+					Qty: 1,
+					Purchased: '',
+					Notes: ''
+				}
+			]
+		});
+	});
+});
+
+describe('WineCellar update', () => {
+	it('should update the wine correctly', () => {
+		const myCellar = new WineCellar();
+		myCellar.addWine('Chateau Margaux', {
+			'Wine Name': 'Margaux 2015',
+			'Vineyard Location': 'Bordeaux, France',
+			Variety: 'Cabernet Sauvignon',
+			Vintage: 2015,
+			Bin: 'A1',
+			Qty: 10,
+			Purchased: '2020-01-01',
+			Notes: 'Excellent vintage'
+		});
+
+		const updatedWine = {
+			'Wine Name': 'Margaux 2015',
+			'Vineyard Location': 'Bordeaux, France',
+			Variety: 'Cabernet Sauvignon',
+			Vintage: 2015,
+			Bin: 'A1',
+			Qty: 5, // Updated quantity
+			Purchased: '2020-01-01',
+			Notes: 'Excellent vintage'
+		};
+
+		const result = myCellar.updateWine('Chateau Margaux', updatedWine);
+
+		expect(result).toBe(true);
+		expect(myCellar.getWinesByProducer('Chateau Margaux')[0].Qty).toBe(5);
+	});
+});
+
+describe('WineCellar getNames', () => {
+	let loadWinesString = `{
+	"B.R. Cohn": [
+		{
+			"Wine Name": "Cabernet Sauvignon",
+			"Vineyard Location": "North Coast",
+			"Variety": "Cabernet Sauvignon",
+			"Vintage": 2013,
+			"Bin": "8-3",
+			"Qty": 1,
+			"Purchased": "",
+			"Notes": ""
+		}
+	],
+	"Barons De Rothschild (Lafite)": [
+		{
+			"Wine Name": "Saint Emilion Reserve Speciale",
+			"Vineyard Location": "Saint Emilion",
+			"Variety": "Red Blend",
+			"Vintage": 2012,
+			"Bin": "5-1",
+			"Qty": 1,
+			"Purchased": "",
+			"Notes": ""
+		}
+	],
+	"Becker Vineyards": [
+		{
+			"Wine Name": "Barbera ",
+			"Vineyard Location": "Texas Hill Country",
+			"Variety": "Barbera",
+			"Vintage": 2020,
+			"Bin": "12-2",
+			"Qty": 1,
+			"Purchased": "10/24/2023",
+			"Notes": ""
+		},
+		{
+			"Wine Name": "Barbera - Merlot",
+			"Vineyard Location": "Texas  ",
+			"Variety": "Red Blend",
+			"Vintage": 2012,
+			"Bin": "15-2",
+			"Qty": 1,
+			"Purchased": "",
+			"Notes": ""
+		}
+	]
+}`;
+	let loadedOwnedWines: Cellar = {};
+
+	try {
+		loadedOwnedWines = JSON.parse(loadWinesString) as Cellar;
+	} catch (error) {
+		console.error(`Error parsing data from demo string:`, error);
+	}
+
+	const myCellar = new WineCellar(loadedOwnedWines);
+
+	it('should return producer names in the form of {name: producerName, value: producerName', () => {
+		const producerNames = myCellar.getProducerNames();
+
+		expect(producerNames).toEqual([
+			{ name: 'B.R. Cohn', value: 'B.R. Cohn' },
+			{ name: 'Barons De Rothschild (Lafite)', value: 'Barons De Rothschild (Lafite)' },
+			{ name: 'Becker Vineyards', value: 'Becker Vineyards' }
+		]);
+	});
+
+	it('should return variety names in the form of {name: varietyName, value: varietyName', () => {
+		const varietyNames = myCellar.getVarietyNames();
+		console.log(varietyNames);
+		expect(varietyNames).toEqual([
+			{ name: 'Cabernet Sauvignon', value: 'Cabernet Sauvignon' },
+			{ name: 'Red Blend', value: 'Red Blend' },
+			{ name: 'Barbera', value: 'Barbera' }
+		]);
+	});
+
+	it('should return vineyard names in the form of {name: vineyard, value: vineyard', () => {
+		const vineyardNames = myCellar.getVineyardNames();
+		console.log(vineyardNames);
+		expect(vineyardNames).toEqual([
+			{ name: 'North Coast', value: 'North Coast' },
+			{ name: 'Saint Emilion', value: 'Saint Emilion' },
+			{ name: 'Texas Hill Country', value: 'Texas Hill Country' },
+			{ name: 'Texas  ', value: 'Texas  ' }
+		]);
+	});
+});
